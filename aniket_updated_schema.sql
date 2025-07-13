922936cfae7960e362d4c8c3f30e553984986380
Aniket Kumar Mishra <149694464+anni990@users.noreply.github.com>
2025-07-14 00:10:48 +0530

    Update aniket_updated_schema.sql

--- aniket_updated_schema.sql
+++ aniket_updated_schema.sql
@@ -3,3 +3,7 @@ ALTER TABLE shopkeepers ADD COLUMN pan_doc_path VARCHAR(255);
 ALTER TABLE shopkeepers ADD COLUMN address_proof_path VARCHAR(255);
 
 ALTER TABLE users ADD COLUMN plain_password VARCHAR(255);
+
+ALTER TABLE chartered_accountants ADD COLUMN gst_number VARCHAR(255);
+ALTER TABLE chartered_accountants ADD COLUMN pan_number VARCHAR(255);
+ALTER TABLE chartered_accountants ADD COLUMN address VARCHAR(255);
